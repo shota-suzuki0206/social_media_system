72e5d76ab7e6a5d109fab27b36b0a6ae30a4136d
shota-suzuki0206 <shota.suzuki0206@gmail.com>
2021-11-20 14:34:26 +0900

    定数Enumクラスの作成

---
+++ src/main/java/constants/AttributeConst.java
@@ -0,0 +1,46 @@
+package constants;
+
+
+/**
+ * 画面の項目値等を定義するEnumクラス
+ *
+ */
+public enum AttributeConst {
+
+    //フラッシュメッセージ
+    FLUSH("flush"),
+
+    //一覧画面共通
+    MAX_ROW("maxRow"),
+    PAGE("page"),
+
+    //入力フォーム共通
+    TOKEN("_token"),
+    ERR("errors"),
+
+    //削除フラグ
+    DEL_FLAG_TRUE(1),
+    DEL_FLAG_FALSE(0);
+
+    private final String text;
+    private final Integer i;
+
+    private AttributeConst(final String text) {
+        this.text = text;
+        this.i = null;
+    }
+
+    private AttributeConst(final Integer i) {
+        this.text = null;
+        this.i = i;
+    }
+
+    public String getValue() {
+        return this.text;
+    }
+
+    public Integer getIntegerValue() {
+        return this.i;
+    }
+
+}
