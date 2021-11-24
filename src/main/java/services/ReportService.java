fff93501f245c3b68e1f7b18e722ea59e3f3fd45
shota-suzuki0206 <shota.suzuki0206@gmail.com>
2021-11-24 18:37:18 +0900

    ReportService作成

---
+++ src/main/java/services/ReportService.java
@@ -0,0 +1,158 @@
+package services;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import actions.views.ReportConverter;
+import actions.views.ReportView;
+import actions.views.UserConverter;
+import actions.views.UserView;
+import constants.JpaConst;
+import models.Report;
+import models.validators.ReportValidator;
+
+/**
+ * 投稿テーブルの操作にかかわる処理を行うクラス
+ *
+ */
+public class ReportService extends ServiceBase {
+
+    /**
+     * 指定したユーザーが作成した投稿データを、指定されたページ数の一覧画面に表示する文取得しReportViewのリストで返却する
+     * @param user ユーザー
+     * @param page ページ数
+     * @return 一覧画面に表示するデータのリスト
+     */
+    public List<ReportView> getMinePerPage(UserView user, int page){
+
+        List<Report> reports = em.createNamedQuery(JpaConst.Q_REP_GET_ALL_MINE, Report.class)
+                .setParameter(JpaConst.JPQL_PARM_USER, UserConverter.toModel(user))
+                .setFirstResult(JpaConst.ROW_PER_PAGE * (page - 1))
+                .setMaxResults(JpaConst.ROW_PER_PAGE)
+                .getResultList();
+        return ReportConverter.toViewList(reports);
+    }
+
+    /**
+     * 指定したユーザーが作成した投稿データの件数を取得し、返却する
+     * @param user
+     * @return 投稿データの件数
+     */
+    public long countAllMine(UserView user) {
+        long count =(long) em.createNamedQuery(JpaConst.Q_REP_COUNT_ALL_MINE, Long.class)
+                .setParameter(JpaConst.JPQL_PARM_USER, UserConverter.toModel(user))
+                .getSingleResult();
+
+        return count;
+    }
+
+    /**
+     * 指定されたページ数の一覧画面に表示する投稿データを取得し、ReportViewのリストで返却する
+     * @param page ページ数
+     * @return 一覧画面に表示するデータのリスト
+     */
+    public List<ReportView> getAllPage(int page){
+
+        List<Report> reports = em.createNamedQuery(JpaConst.Q_REP_GET_ALL, Report.class)
+                .setFirstResult(JpaConst.ROW_PER_PAGE * (page - 1))
+                .setMaxResults(JpaConst.ROW_PER_PAGE)
+                .getResultList();
+        return ReportConverter.toViewList(reports);
+    }
+
+    /**
+     * 投稿テーブルのデータの件数を取得し、返却する
+     * @return データの件数
+     */
+    public long countAll() {
+        long reports_count = (long)em.createNamedQuery(JpaConst.Q_REP_COUNT, Long.class)
+                .getSingleResult();
+        return reports_count;
+    }
+
+    /**
+     * idを条件に取得したデータをReportViewのインスタンスで返却する
+     * @param id
+     * @return 取得データのインスタンス
+     */
+    public ReportView findOne(int id) {
+        return ReportConverter.toView(findOneInternal(id));
+    }
+
+    /**
+     * 画面から入力された投稿内容を元にデータを1件作成し、投稿テーブルに登録する
+     * @param rv 投稿内容
+     * @return バリデーションで発生したエラーのリスト
+     */
+    public List<String> create(ReportView rv){
+        List<String> errors = ReportValidator.validate(rv);
+        if(errors.size() == 0) {
+            LocalDateTime ldt = LocalDateTime.now();
+            rv.setCreatedAt(ldt);
+            rv.setUpdatedAt(ldt);
+            createInternal(rv);
+
+        }
+
+      //バリデーションで発生したエラーを返却（エラーがなければ0件の空リスト）
+        return errors;
+    }
+
+    /**
+     * 画面から入力された投稿内容を元に、投稿データを更新する
+     * @param rv 投稿の更新内容
+     * @return バリデーションで発生したエラーのリスト
+     */
+    public List<String> update(ReportView rv) {
+
+        //バリデーションを行う
+        List<String> errors = ReportValidator.validate(rv);
+
+        if (errors.size() == 0) {
+
+            //更新日時を現在時刻に設定
+            LocalDateTime ldt = LocalDateTime.now();
+            rv.setUpdatedAt(ldt);
+
+            updateInternal(rv);
+        }
+
+        //バリデーションで発生したエラーを返却（エラーがなければ0件の空リスト）
+        return errors;
+    }
+
+    /**
+     * idを条件にデータを1件取得する
+     * @param id
+     * @return 取得データのインスタンス
+     */
+    private Report findOneInternal(int id) {
+        return em.find(Report.class, id);
+    }
+
+    /**
+     * 投稿データを1件登録する
+     * @param rv 投稿データ
+     */
+    private void createInternal(ReportView rv) {
+
+        em.getTransaction().begin();
+        em.persist(ReportConverter.toModel(rv));
+        em.getTransaction().commit();
+
+    }
+
+    /**
+     * 投稿データを更新する
+     * @param rv 投稿データ
+     */
+    private void updateInternal(ReportView rv) {
+
+        em.getTransaction().begin();
+        Report r = findOneInternal(rv.getId());
+        ReportConverter.copyViewToModel(r, rv);
+        em.getTransaction().commit();
+
+    }
+
+}
